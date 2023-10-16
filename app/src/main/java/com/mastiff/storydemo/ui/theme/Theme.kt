df82758732d00368e15a30881ea6b7833a7a58d0
Mastiff <mastiffzone365@gmail.com>
2023-10-17 02:45:14 +0800

    transparent status bar

--- app/src/main/java/com/mastiff/storydemo/ui/theme/Theme.kt
+++ app/src/main/java/com/mastiff/storydemo/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.mastiff.storydemo.ui.theme
 
 import android.app.Activity
+import android.graphics.Color
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -56,8 +56,10 @@ fun StoryDemoTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            val insets = WindowCompat.getInsetsController(window, view)
+            window.statusBarColor = Color.TRANSPARENT
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            insets.isAppearanceLightStatusBars = true
         }
     }
 
